c7e776e2535c21a3caca8616d8760024901d1f21
AlexandreDev <eztalenda@gmail.com>
2022-12-22 03:57:20 -0300

    feat(main): Adiciona lib axios e implementa primeira request de get para listagem. - Modifica CSS para adaptar layout inicial de cards.

---
+++ src/API.js
@@ -0,0 +1,2 @@
+export const API_URL =
+  'https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=AIzaSyC3IEghsG4m5EG2kqPeh8_VgRQOc_lJkfA'
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../App'
+import { API_URL } from '../API'
+import axios from 'axios'
 
 const BookList = () => {
-  return <div className="book-list">BookList</div>
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((res) => {
+        console.log(res.data)
+        setBooks(res.data.items)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  return (
+    <div className="book-list">
+      {books.map((book) => (
+        <div key={book.id} className="book-card">
+          <div>
+            <h4>{book.volumeInfo.title}</h4>
+          </div>
+          <div>
+            <img src={book.volumeInfo.imageLinks.thumbnail} alt="imagem" />
+            <div>
+              <button>Adicionar aos Favoritos</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default BookList
